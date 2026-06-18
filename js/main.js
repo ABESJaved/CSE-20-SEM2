0e43534a3e16e6801cc1d802fa876adc8bfe40e2
ABESJaved <javedahmad25700@gmail.com>
2026-06-19 00:28:09 +0530

    hi

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ let nums = [5, 2, 8, 1];
 console.log(x > y ? x : y);
 let value = -5;
 for (let i = 1; i <= 10; i++) 
+    let str = "hello";
+console.log(str.split("").reverse().join(""));
+console.log(str.length);
+let arr = [1, 2, 3, 4];
